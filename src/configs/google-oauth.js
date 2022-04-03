ffd58ab29a3dcf2211592d47724c0a5260a85050
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-03 15:48:42 +0530

    return to previous

--- src/configs/google-oauth.js
+++ src/configs/google-oauth.js
@@ -14,37 +14,36 @@ const newToken=(user)=>{
 
 const User=require("../models/user.model");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://clonecoursera.netlify.app/auth/google/callback",
-      profileFields: ["id", "displayName", "photos", "email"],
-    },
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/callback",
+    profileFields: ['id', 'displayName', 'photos', 'email']
+  },
 
-    async function (accessToken, refreshToken, profile, callback) {
-      try {
-        //   console.log(accessToken,profile,callback)
-        let user = await User.findOne({ email: profile?._json?.email })
-          .lean()
-          .exec();
+  async function(accessToken, refreshToken, profile, callback) {
+    try {
+      //   console.log(accessToken,profile,callback)
+      let user= await User.findOne({email:profile?._json?.email}).lean().exec()
+ 
+      if(!user){
+            user= await User.create({
+               username:profile._json.name,
+               email:profile._json.email,
+               password:uuidv4()
+       
+           })
+       }
+      
+      //    console.log(user)
+      let token = newToken(user)
+       return callback(null,user,token);
+     
+   } catch (error) {
+       console.log(error)
+   }
 
-        if (!user) {
-          user = await User.create({
-            username: profile._json.name,
-            email: profile._json.email,
-            password: uuidv4(),
-          });
-        }
-
-        //    console.log(user)
-        let token = newToken(user);
-        return callback(null, user, token);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  )
-);
+  
+}
+));
 module.exports= passport;
