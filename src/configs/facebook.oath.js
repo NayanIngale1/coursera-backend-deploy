9d535550fc6e01fe8d0c0f2571c926ede891a7b7
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-03 15:53:40 +0530

    normal update

--- src/configs/facebook.oath.js
+++ src/configs/facebook.oath.js
@@ -11,35 +11,36 @@ const newToken=(user)=>{
 
 const User=require("../models/user.model")
 
-passport_fb.use(new FacebookStrategy({
-    clientID:  1184217039016257,
-    clientSecret:"bcaba0960ce0984e8cf4efc9de70c0d6",
-    callbackURL: "http://localhost:5000/auth/facebook/callback",
-    profileFields: ['id', 'displayName', 'photos', 'email']
-  },
-  async function(accessToken, refreshToken, profile, callback) {
-     try {
+passport_fb.use(
+  new FacebookStrategy(
+    {
+      clientID: 1184217039016257,
+      clientSecret: "bcaba0960ce0984e8cf4efc9de70c0d6",
+      callbackURL: "https://clonecoursera.netlify.app/auth/facebook/callback",
+      profileFields: ["id", "displayName", "photos", "email"],
+    },
+    async function (accessToken, refreshToken, profile, callback) {
+      try {
         //   console.log(accessToken,profile,callback)
-        let user= await User.findOne({email:profile?._json?.email}).lean().exec()
-   
-        if(!user){
-              user= await User.create({
-                 username:profile._json.name,
-                 email:profile._json.email,
-                 password:uuidv4()
-         
-             })
-         }
-        
-        //    console.log(user)
-        let token = newToken(user)
-         return callback(null,user,token);
-       
-     } catch (error) {
-         console.log(error)
-     }
+        let user = await User.findOne({ email: profile?._json?.email })
+          .lean()
+          .exec();
 
-    
-}
-));
+        if (!user) {
+          user = await User.create({
+            username: profile._json.name,
+            email: profile._json.email,
+            password: uuidv4(),
+          });
+        }
+
+        //    console.log(user)
+        let token = newToken(user);
+        return callback(null, user, token);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
 module.exports= passport_fb
