456b5e19978adcfcae7cfbd0662f479dadb17a24
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-03 14:04:47 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const app = require("./index");
 const connect = require("./configs/db")
 
+const port = process.env.PORT || 5000
 
-app.listen(5000, async () => {
+app.listen(port, async () => {
   try {
     await connect();
-    console.log("listening on port 5000");
+    console.log(`listening on port ${port}`);
   } catch (err) {
     console.log(err.message);
   }
